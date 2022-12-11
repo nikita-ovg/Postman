aff137d011899cfb371b64423c9d10ac4a5dfe3d
Nikita Grebennik <97547224+nikita-ovg@users.noreply.github.com>
2022-12-11 21:22:36 +0300

    1

--- HW_3/Task_1.js
+++ HW_3/Task_1.js
@@ -1,10 +1,10 @@
-**Responce**
-  ```sh
+Responce
 {
     "token": "/s34lfgbj/hrfjrs362/jjd909/36337kjkWpqc5402kmtltluktkr36226404821evny"
 }
-```
 
-**Tests**
+
+Tests
+
 var kil = pm.response.json();
 pm.environment.set("token", kil.token);
